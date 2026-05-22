664f11c980bc66c33592468b5954c045a48d211f
Marcelo Brigato <marcelo.brigato_c@tdmsystems.com>
2026-05-22 16:03:04 -0300

    fix: add sys.path insertion for module resolution in alembic env.py

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,4 +1,7 @@
 import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from logging.config import fileConfig
 from sqlalchemy import engine_from_config, pool
 from alembic import context
